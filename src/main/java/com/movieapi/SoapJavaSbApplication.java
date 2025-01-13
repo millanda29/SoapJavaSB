8cabf1230d1c2e4c32681983779a50d1d3c306fd
millanda29 <maikolllanda@gmail.com>
2025-01-13 08:05:41 -0500

    feat: 🎉 first commit

---
+++ src/main/java/com/movieapi/SoapJavaSbApplication.java
@@ -0,0 +1,13 @@
+package com.movieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapJavaSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapJavaSbApplication.class, args);
+    }
+
+}
